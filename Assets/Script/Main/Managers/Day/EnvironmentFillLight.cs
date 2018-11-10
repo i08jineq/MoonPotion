fc994e6766f81c1de2fe80857a0037bc13e876c5
ELEQ <>
2018-11-11 00:06:03 +0900

    added light data

---
+++ Assets/Script/Main/Managers/Day/EnvironmentFillLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DarkLordGame
+{
+    public class EnvironmentFillLight : MonoBehaviour
+    {
+        public Light lightObject;
+        public Gradient dayColor;
+        public AnimationCurve lightIntensityCurve;
+
+        public virtual void UpdateTime(float time)
+        {
+            Color color = dayColor.Evaluate(time);
+            float lightIntensity = lightIntensityCurve.Evaluate(time);
+            lightObject.intensity = lightIntensity;
+            lightObject.color = color;
+        }
+    }
+}
---
+++ Assets/Script/Main/Managers/Day/EnvironmentSpotLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DarkLordGame
+{
+    public class EnvironmentSpotLight : EnvironmentFillLight
+    {
+        public AnimationCurve lightAngleCurve;
+
+        /// <summary>
+        /// time  0 ~ 1
+        /// </summary>
+        public override void UpdateTime(float time)
+        {
+            base.UpdateTime(time);
+            Vector3 eurler = transform.eulerAngles;
+            float lightAngleY = lightAngleCurve.Evaluate(time);
+            eurler.y = lightAngleY;
+            transform.eulerAngles = eurler;
+        }
+    }
+}
