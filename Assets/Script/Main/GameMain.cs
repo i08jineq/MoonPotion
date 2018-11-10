0edcb779a04afa4cfb2c9b1fa8eab1f8803e1a63
ELEQ <>
2018-11-10 11:12:47 +0900

    maded manager

--- Assets/Script/Main/GameMain.cs
+++ Assets/Script/Main/GameMain.cs
@@ -7,23 +7,40 @@ namespace DarkLordGame
     public class GameMain : MonoBehaviour
     {
         public FadeLayer fadeLayer;
-
+        private Day day;
+        private bool shouldExecuteDay = false;
+        #region initialization
         private void Awake()
         {
             fadeLayer.ForceColor(Color.black);
         }
 
         public IEnumerator Start()
-		{
+        {
             yield return Model.Init();
 
             fadeLayer.FadeIn();
-		}
+        }
 
+        private void SetupDay()
+        {
+            day = new Day();
+        }
 
-		private void Update()
-		{
-            
-		}
-	}
+        #endregion
+
+        private void Update()
+        {
+            float deltaTime = Time.deltaTime;
+            UpdateDay(deltaTime);
+        }
+
+        private void UpdateDay(float deltaTime)
+        {
+            if(shouldExecuteDay)
+            {
+                day.OnUpdate(deltaTime);
+            }
+        }
+    }
 }
